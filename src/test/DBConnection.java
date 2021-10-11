cf614e6d77620ee52be657f5d60973315f0f1eba
rachit <sethirachit1996@gmail.com>
2021-10-11 11:27:47 +0000

    Ecommerceproject commit

--- src/test/DBConnection.java
+++ src/test/DBConnection.java
@@ -1,5 +1,26 @@
 package test;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBConnection {
+         
+	Connection con;
+
+	public DBConnection(String URL,String UserName, String Paasword) throws ClassNotFoundException, SQLException {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		this.con = DriverManager.getConnection(URL, UserName, Paasword);
+
+}
+    
+	public Connection getConnection() {
+		return this.con;
+	}
+
+	public void closeConnection() throws SQLException{
+		if(this.con != null)
+			this.con.close();
+	}
 }
